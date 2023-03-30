3f1cce50274820f079beb9e3a38cfb370def9aca
prkashp <prakashpro86@gmail.com>
2023-03-31 00:43:03 +0530

    added main file

--- text-cleaner/app/util/extractor.py
+++ text-cleaner/app/util/extractor.py
@@ -6,6 +6,3 @@ def getText(url):
     text.parse()
     output = [text.authors,text.publish_date,text.text,text.summary,text.keywords]
     return output
-url = "https://en.wikipedia.org/wiki/Bongcloud_Attack"
-
-print(getText(url))
